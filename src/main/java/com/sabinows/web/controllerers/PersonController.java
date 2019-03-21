b7e913fc7d7f481a1cc820aa649e097e96957711
Sabino <sabino.th@hotmail.com>
2019-03-21 10:27:04 -0300

    Iniciando formulário de cadastro de pessoa + Removendo rota home

--- src/main/java/com/sabinows/web/controllerers/PersonController.java
+++ src/main/java/com/sabinows/web/controllerers/PersonController.java
@@ -2,12 +2,23 @@ package com.sabinows.web.controllerers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sabinows.domain.Person;
 
 @Controller
 public class PersonController {
 
 	@GetMapping("/")
-	public String home() {
+	public String register(Person person) {
 		return "/person/register";
+	}
+	
+	@PostMapping("/save")
+	public String save(RedirectAttributes attr) {
+		// TO DO
+		attr.addFlashAttribute("success", "Em construção.");
+		return "redirect:/";
 	}
 }
